50a24d433469482955e9b532913093ea13f5bb69
NodirQurbonbayev <yusupovn948@gmail.com>
2023-06-08 20:39:37 +0500

    first code

--- main.go
+++ main.go
@@ -257,3 +257,18 @@ func main() {
 	sum := ac + bc
 	fmt.Println(sum)
 }
+package main 
+
+import (
+	"fmt"
+	"math"
+)
+func main() {
+	var x1,x2,y1,y2 int 
+	fmt.Scan("x1="&x1)
+	fmt.Scan("x2="&x2)
+	fmt.Scan("y1="&y1)
+	fmt.Scan("y2="&y2)
+	sum:=math.Sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1))
+	fmt.Println(sum)
+}
