90f0311ff5d278f7ffe00ccd123e545533b549a8
NodirQurbonbayev <yusupovn948@gmail.com>
2023-06-08 19:44:38 +0500

    first code

--- main.go
+++ main.go
@@ -9,3 +9,14 @@ func main() {
 	var p int = 4 * a
 	fmt.Println("Perimeter (p) =", p)
 }
+
+package main
+
+import "fmt"
+//Yuzini topish
+func main()  {
+	var a,S int 
+	fmt.Scan(&a)
+	S=a*a
+	fmt.Println("Yuzi=",S)
+}
